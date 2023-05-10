ac3dcc798194599990da198dd0e78932962d29a2
luiswolff <luis-manuel.wolff@outlook.com>
2023-05-10 12:02:00 +0200

    refactored test code

--- src/test/java/io/github/luiswolff/calc/SampleCalculatorTest.java
+++ src/test/java/io/github/luiswolff/calc/SampleCalculatorTest.java
@@ -1,49 +1,89 @@
 package io.github.luiswolff.calc;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.awt.AWTException;
-import java.awt.Component;
-import java.awt.Container;
-import java.awt.Robot;
+import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.util.HashMap;
 import java.util.Map;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class SampleCalculatorTest {
 
-  @Test
-  void test() throws InterruptedException, AWTException {
-    SampleCalculator calc = new SampleCalculator();
-    calc.setVisible(true);
-    calc.setLocation(100, 50);
-    Thread.sleep(1000L);
-    Component component = extracted(calc).get("button-1");
-    System.out.println("Pos in App " + component.getLocation());
-    System.out.println("Pos on Screen " + component.getLocationOnScreen());
-    System.out.println("Size " + component.getSize());
+  private SampleCalculator underTest;
+  private Map<String, Component> componentByNameIndex;
+  private Robot robot;
 
-    Robot robot = new Robot();
-    robot.mouseMove(component.getLocationOnScreen().x + component.getSize().width / 2, component.getLocationOnScreen().y + component.getSize().height / 2);
-    robot.mousePress(MouseEvent.BUTTON1_MASK);
-    robot.mouseRelease(MouseEvent.BUTTON1_MASK);
-    Thread.sleep(1000L);
-    calc.dispose();
+  @BeforeEach
+  void setUp() {
+    setUpUnderTest();
+    setUpRobot();
   }
 
-  private static Map<String, Component> extracted(Component comp) {
+  private void setUpUnderTest() {
+    underTest = new SampleCalculator();
+    underTest.setVisible(true);
+    componentByNameIndex = indexComponents(underTest);
+  }
+
+  private static Map<String, Component> indexComponents(Component comp) {
     Map<String, Component> result = new HashMap<>();
     if (comp instanceof Container) {
       Container cont = (Container) comp;
       for (int i = 0; i < cont.getComponentCount(); i++) {
-        result.putAll(extracted(cont.getComponent(i)));
+        result.putAll(indexComponents(cont.getComponent(i)));
       }
     }
     if (comp.getName() != null) {
       result.put(comp.getName(), comp);
     }
     return result;
+  }
+
+  private void setUpRobot() {
+    try {
+      robot = new Robot();
+      robot.setAutoWaitForIdle(true);
+      robot.setAutoDelay(250);
+    } catch (AWTException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @AfterEach
+  void tearDown() {
+    if (underTest != null) {
+      underTest.dispose();
+    }
+  }
+
+  @Test
+  void test() throws InterruptedException {
+    clickOnComponent("button-1");
+    clickOnComponent("button-+");
+    clickOnComponent("button-2");
+    clickOnComponent("button-=");
+  }
+
+  private void clickOnComponent(String componentName) throws InterruptedException {
+    Component component = componentByNameIndex.get(componentName);
+    Point componentsLocationOnScreen = middleOf(component);
+    clickAt(componentsLocationOnScreen);
+  }
+
+  private static Point middleOf(Component component) {
+    Point locationOnScreen = new Point(component.getLocationOnScreen());
+    Dimension size = component.getSize();
+    locationOnScreen.translate(size.width / 2, size.height / 2);
+    return locationOnScreen;
+  }
+
+  private void clickAt(Point componentsLocationOnScreen) throws InterruptedException {
+    robot.mouseMove(componentsLocationOnScreen.x, componentsLocationOnScreen.y);
+    robot.mousePress(MouseEvent.BUTTON1_DOWN_MASK);
+    robot.mouseRelease(MouseEvent.BUTTON1_DOWN_MASK);
+    Thread.sleep(1000L);
   }
 
 }
